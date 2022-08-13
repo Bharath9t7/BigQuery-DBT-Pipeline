f8ab0dcb697618ecb99ba4cdb65340e6387824be
Bharath9t7 <39058992+Bharath9t7@users.noreply.github.com>
2022-08-14 00:00:27 +0530

    Added ingestion_timestamp column to all staging tables

--- titan_analytics/models/staging/stg_privileges.sql
+++ titan_analytics/models/staging/stg_privileges.sql
@@ -1,4 +1,6 @@
 with source as (
     select * from {{ source('northwind', 'privileges') }}
 )
-select * from source
+select *,
+       current_timestamp() as ingestion_timestamp
+  from source
--- titan_analytics/models/staging/stg_strings.sql
+++ titan_analytics/models/staging/stg_strings.sql
@@ -1,4 +1,6 @@
 with source as (
     select * from {{ source('northwind', 'strings') }}
 )
-select * from source
+select *,
+       current_timestamp() as ingestion_timestamp
+  from source
